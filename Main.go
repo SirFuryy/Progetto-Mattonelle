5a8104187dda3e7e879e2644736a6136e2fb31b2
SirFuryy <giacomotommi@gmail.com>
2024-06-19 14:34:05 +0200

    creata funzione che calcola la pista

--- Main.go
+++ Main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 )
 
+/* STRUTTURE DATI*/
+
 //struttura dati per rappresentare un punto su un piano cartesiano
 type Punto struct { 
-	x int
-	y int 
+	x int	//colonna
+	y int 	//riga
 }
 
 // struttura dati per rappresentare una piastrella, centrata all'interno 
@@ -33,68 +35,67 @@ type Piano struct {
 	regole []string
 }
 
+
+
+
 func main() {
 	piano := creaPiano(6)
 
-	colora(piano,1, 2, "f", 48)
-	colora(piano,3, 5, "o", 66)
-	colora(piano,4, 3, "r", 18)
-	colora(piano,5, 1, "j", 5)	
-	colora(piano,2, 0, "g", 26)
-	colora(piano,4, 0, "x", 45)
-	colora(piano,4, 1, "j", 6)
-	colora(piano,0, 0, "d", 77)
-	colora(piano,1, 5, "m", 79)
-	colora(piano,0, 2, "e", 71)
-	colora(piano,4, 4, "g", 6)
-	colora(piano,0, 1, "j", 47)
-	colora(piano,1, 3, "o", 43)
-	colora(piano,3, 1, "h", 68)
-	colora(piano,4, 5, "c", 50)
-	colora(piano,1, 5, "e", 66)
-	colora(piano,2, 4, "m", 49)
-	colora(piano,2, 2, "g", 96)
-	colora(piano,5, 0, "h", 28)
-	colora(piano,3, 2, "f", 56)
+	colora(piano, 3, 5, "x", 96)
+	colora(piano, 1, 3, "h", 90)
+	colora(piano, 2, 1, "s", 44)
+	colora(piano, 2, 3, "o", 78)
+	colora(piano, 0, 5, "f", 6)
+	colora(piano, 0, 2, "a", 46)
+	colora(piano, 5, 1, "i", 45)
+	colora(piano, 4, 1, "i", 27)
+	colora(piano, 0, 1, "f", 18)
+	colora(piano, 2, 2, "d", 59)
+	colora(piano, 5, 5, "u", 69)
+	colora(piano, 0, 3, "t", 13)
 	
 
 	stampaPiano(piano)
 
-	fmt.Println("Accesa: ", Accesa(piano.piastrelle[1][2]))
-	fmt.Println("Accesa: ", Accesa(piano.piastrelle[1][4]))
-	fmt.Println("Circonvicini: ", circonvicini(piano.piastrelle[1][2], piano.piastrelle[1][3]))
-	fmt.Println("Circonvicini: ", circonvicini(piano.piastrelle[1][2], piano.piastrelle[1][4]))
+	pista(piano, 0, 2, "NEEESSNOOO")
 }
+
+
+
+/* FUNZIONI DI STAMPA*/
+
 
 //funzione di utilità che stampa lo stato del piano, con il colore delle piastrelle
 //attualmente accese (non l'intensità)
 func stampaPiano(piano Piano) {
 	fmt.Println("  ^")
 	fmt.Println("  |")
-	str := "  |"
+	str := "|"
 	for i:=0; i<len(piano.piastrelle); i++ {
 		str += "---|"
 	}
-	fmt.Println(str)
+	fmt.Println("  "+ str)
 
 	for i := len(piano.piastrelle)-1; i >=0 ; i-- {
-		fmt.Printf("%d |", i)
+		fmt.Print("  |")
 		for j := 0; j < len(piano.piastrelle[i]); j++ {
-			fmt.Print(" " + piano.piastrelle[i][j].colore + " |")
-		}
+			fmt.Print(" " + piano.piastrelle[j][i].colore + " |")	//lettere sono invertite
+		}													//perchè la matrice è invertita
 		fmt.Println()
 		if i == 0 {
-			fmt.Println(str+"->")
+			fmt.Println(i, str+"->")
 		} else {
-			fmt.Println(str)
+			fmt.Println(i, str)
 		}
 	}
-	fmt.Print("    ")
+	fmt.Print("  ")
 	for i:= 0; i < len(piano.piastrelle); i++ {
 		fmt.Print(i, "   ")
 	}
 	fmt.Println()
 }
+
+/* FUNZIONI DI COSTRUZIONE */
 
 
 //funzione costruttrice che crea un piano di piastrelle, con n piastrelle per lato
@@ -111,34 +112,80 @@ func creaPiano(n int) Piano{
 }
 
 //funzione costruttrice che crea i circonvicini di ogni piastrella del piano
+//l'array conterrà le piastrelle circonvicine ordinate a partire dalla 
+//posizione (x-1, y+1) [per intenderci la posizione NO] e proseguendo in 
+//ordine di riga e colonna, ovvero dato un punto (x,y) al centro del piano con
+//le posizioni vicine disposte come segue:
+//      x-1, y+1 | x, y+1 | x+1, y+1
+//      x-1, y   | x, y   | x+1, y
+//      x-1, y-1 | x, y-1 | x+1, y-1
+//la posizione dei circonvicini sarà disposta come segue:
+//		  [0]	 |   [1]  |    [2] 
+//        [3]	 |   [4]  |    [5]
+//        [6]	 |   [7]  |    [8]
 func creaCirconvicini(piano [][]Piastrella, n int) [][]Piastrella{
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			circonvicini := make([]*Piastrella, 0)
-			if i > 0 {
-				circonvicini = append(circonvicini, &piano[i-1][j])
-				if j > 0 {
-					circonvicini = append(circonvicini, &piano[i-1][j-1])
-				}
-				if j < n-1 {
-					circonvicini = append(circonvicini, &piano[i-1][j+1])
-				}
+			circonvicini := make([]*Piastrella, 0, 9)
+			//x-1, y+1
+			if i > 0 && j < n-1 {
+				circonvicini = append(circonvicini, &piano[i-1][j+1])
+			} else {
+				circonvicini = append(circonvicini, nil)
 			}
-			if i < n-1 {
-				circonvicini = append(circonvicini, &piano[i+1][j])
-				if j > 0 {
-					circonvicini = append(circonvicini, &piano[i+1][j-1])
-				}
-				if j < n-1 {
-					circonvicini = append(circonvicini, &piano[i+1][j+1])
-				}
-			}
-			if j > 0 {
-				circonvicini = append(circonvicini, &piano[i][j-1])
-			}
+
+			//x, y+1
 			if j < n-1 {
 				circonvicini = append(circonvicini, &piano[i][j+1])
+			} else {
+				circonvicini = append(circonvicini, nil)
 			}
+
+			//x+1, y+1
+			if i < n-1 && j < n-1 {
+				circonvicini = append(circonvicini, &piano[i+1][j+1])
+			} else {
+				circonvicini = append(circonvicini, nil)
+			}
+
+			//x-1, y
+			if i > 0 {
+				circonvicini = append(circonvicini, &piano[i-1][j])
+			} else {
+				circonvicini = append(circonvicini, nil)
+			}
+
+			//x, y
+			circonvicini = append(circonvicini, &piano[i][j])
+
+			//x+1, y
+			if i < n-1 {
+				circonvicini = append(circonvicini, &piano[i+1][j])
+			} else {
+				circonvicini = append(circonvicini, nil)
+			}
+
+			//x-1, y-1
+			if i > 0 && j > 0 {
+				circonvicini = append(circonvicini, &piano[i-1][j-1])
+			} else {
+				circonvicini = append(circonvicini, nil)
+			}
+
+			//x, y-1
+			if j > 0 {
+				circonvicini = append(circonvicini, &piano[i][j-1])
+			} else {
+				circonvicini = append(circonvicini, nil)
+			}
+
+			//x+1, y-1
+			if i < n-1 && j > 0 {
+				circonvicini = append(circonvicini, &piano[i+1][j-1])
+			} else {
+				circonvicini = append(circonvicini, nil)
+			}
+
 			piano[i][j].circonvicini = circonvicini
 		}
 	}
@@ -152,6 +199,18 @@ func creaPiastrella(i, j int) Piastrella {
 		" ",
 		0, 
 		nil}
+}
+
+//funzione di utilità che stampa tutte le caratteristiche di una piastrella
+func stampaPiastrella(piast Piastrella) {
+	str := "Piastrella di punti:"
+
+	for _, v := range piast.punti {
+		str += fmt.Sprintf(" (%d, %d)", v.x, v.y)
+	}
+
+	str += ", colore: '" + piast.colore + "' e intensità: " + fmt.Sprint(piast.intenisita)
+	fmt.Println(str)
 }
 
 //funzione di utilità che restituisce true se la piastrella è accessa, 
@@ -180,6 +239,9 @@ func trovaRegione(piano Piano, x, y int) []Piastrella {
 		u := visiting[0]
 		visiting = visiting[1:]
 		for i := 0; i < len(u.circonvicini); i++ {
+			if u.circonvicini[i] == nil {
+				continue
+			}
 			pcontrollo := *u.circonvicini[i]
 			if Accesa(pcontrollo) {
 				nonvisit := true
@@ -269,7 +331,97 @@ func ordina(piano Piano) {
 //Stampa la pista che parte da Piastrella(x, y) e segue la sequenza di 
 //direzioni s, se tale pista è definita. Altrimenti non stampa nulla.
 func pista(piano Piano, x, y int, s string) {
-	
+	indicazioni := make([]string, 0)
+	for i := 0; i < len(s); i += 2 {
+		indicazioni = append(indicazioni, s[i : i+2])
+	}
+
+	piastControllata := piano.piastrelle[x][y]
+	if !Accesa(piastControllata) {
+		return
+	}
+	piastrelle := make([]Piastrella, 0)
+	piastrelle = append(piastrelle, piastControllata)
+	for i := 0; i < len(indicazioni); i++ {
+		switch indicazioni[i] {
+		case "NO":	//x-1, y+1
+			ppros:= piastControllata.circonvicini[0]
+			if ppros != nil {
+				if Accesa(*ppros) {
+					piastControllata = *ppros
+					piastrelle = append(piastrelle, piastControllata)
+				} else {return}
+			} else {return}
+			break
+		case "NN":	//x, y+1
+			ppros:= piastControllata.circonvicini[1]
+			if ppros != nil {
+				if Accesa(*ppros) {
+					piastControllata = *ppros
+					piastrelle = append(piastrelle, piastControllata)
+				}  else {return}
+			} else {return}
+			break
+		case "NE":	//x+1, y+1
+			ppros:= piastControllata.circonvicini[2]
+			if ppros != nil {
+				if Accesa(*ppros) {
+					piastControllata = *ppros
+					piastrelle = append(piastrelle, piastControllata)
+				} else {return}
+			} else {return}
+			break
+		case "OO":	//x-1, y
+			ppros:= piastControllata.circonvicini[3]
+			if ppros != nil {
+				if Accesa(*ppros) {
+					piastControllata = *ppros
+					piastrelle = append(piastrelle, piastControllata)
+				} else {return}
+			} else {return}
+			break
+		case "EE":	//x+1, y
+			ppros:= piastControllata.circonvicini[5]
+			if ppros != nil {
+				if Accesa(*ppros) {
+					piastControllata = *ppros
+					piastrelle = append(piastrelle, piastControllata)
+				} else {return}
+			} else {return}
+			break
+		case "SO":	//x-1, y-1
+			ppros:= piastControllata.circonvicini[6]
+			if ppros != nil {
+				if Accesa(*ppros) {
+					piastControllata = *ppros
+					piastrelle = append(piastrelle, piastControllata)
+				} else {return}
+			} else {return}
+			break
+		case "SS":	//x, y-1
+			ppros:= piastControllata.circonvicini[7]
+			if ppros != nil {
+				if Accesa(*ppros) {
+					piastControllata = *ppros
+					piastrelle = append(piastrelle, piastControllata)
+				} else {return}
+			} else {return}
+			break
+		case "SE":	//x+1, y-1
+			ppros:= piastControllata.circonvicini[8]
+			if ppros != nil {
+				if Accesa(*ppros) {
+					piastControllata = *ppros
+					piastrelle = append(piastrelle, piastControllata)
+				} else {return}
+			} else {return}
+			break		
+		}
+	}
+
+	for i := 0; i < len(piastrelle); i++ {
+		stampaPiastrella(piastrelle[i])
+	}
 }
 
 //Determina la lunghezza della pista più breve che parte da Piastrella(x1, y1)
